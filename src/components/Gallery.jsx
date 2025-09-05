e2cf21cf7886f733c73907b0f0a96f11e40885c7
Piyush_Chavan <piyushchavangd2004@gmail.com>
2025-09-05 18:06:34 +0530

    Research tab completed , gallery images added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -16,6 +16,20 @@ export default function Gallery() {
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="7" aria-label="Slide 8"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="8" aria-label="Slide 9"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="9" aria-label="Slide 10"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="10" aria-label="Slide 11"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="11" aria-label="Slide 12"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="12" aria-label="Slide 13"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="13" aria-label="Slide 14"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="14" aria-label="Slide 15"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="15" aria-label="Slide 16"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="16" aria-label="Slide 17"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="17" aria-label="Slide 18"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="18" aria-label="Slide 19"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="19" aria-label="Slide 20"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="20" aria-label="Slide 21"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="21" aria-label="Slide 22"></button>
 
           </div>
           <div class="carousel-inner" style={{ height: '100%' }}>
@@ -56,6 +70,76 @@ export default function Gallery() {
             </div>
             <div class="carousel-item" style={{ height: '100%' }}>
               <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-8.jpg')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-9.jpg')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-10.jpg')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-11.jpg')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-12.jpg')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-13.jpg')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-14.png')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-15.png')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-16.png')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-17.png')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-18.png')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-19.png')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-20.png')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-21.png')} class="d-block" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+              </div>
+            </div>
+            <div class="carousel-item" style={{ height: '100%' }}>
+              <img style={{ borderRadius: '20px', margin: 'auto' }} height='100%' src={require('../assets/gallery-images/img-22.png')} class="d-block" alt="..." />
               <div class="carousel-caption d-none d-md-block">
               </div>
             </div>
--- src/components/ResearchProjects.jsx
+++ src/components/ResearchProjects.jsx
@@ -4,12 +4,78 @@ import './styles.css';
 export default function ResearchProjects() {
   return (
     <div id='research-projects'>
-        <p className='section-title'>Research Projects</p>
-      <ul className="text-1 list-1" style={{listStyle:'number',fontSize:'1rem'}}>
-        <li><b>Hindon Rootz Sensing (HIROS): </b> River Rejuvenation through Scalable Water and Solute Balance Modelling and Informed Farmers Actions (2023); Sponsored by DST, New Delhi under National Mission on Himalayan Ganga and Water Call. Budget: ₹576.5 Lakhs (Includes own part contribution of ₹125.5 Lakhs)</li> <br />
+      <p className='section-title'>Research Projects</p>
+      <ul className="text-1 list-1" style={{ listStyle: 'number', fontSize: '1rem' }}>
+        <li><b style={{ fontSize: '1.4rem' }}>Hindon Roots Sensing (HIROS): </b>
+          <br />
+          River Rejuvenation through Scalable Water and Solute Balance Modelling and Informed Farmers Actions (2023); Sponsored by DST, New Delhi under National Mission on Himalayan Ganga and Water Call. Budget: ₹576.5 Lakhs (Includes own part contribution of ₹125.5 Lakhs)</li> <br />
         {/* <iframe src="https://www.hiros.in/" frameborder="0"></iframe> */}
-        <li><b>Hydrological Modelling for Dynamic Catchments (2021):</b> Funded by Sponsored Research & Industrial Consultancy Office, Indian Institute of Technology Roorkee. Budget: ₹21.6 Lakhs</li> <br />
-        <li><b>Design flood estimation for resilient hydraulic structures under changing climate and environments (2022): </b> Funded by SERB India under Startup Research Grant Scheme. Budget: ₹19.6 Lakhs</li> <br />
+        <p>For more details visit  <a target='_blank' href="https://www.hiros.in/">https://www.hiros.in/</a></p>
+        <p className="section-title" style={{ fontSize: '20px' }}>Project Mission and Vision</p>
+        <div style={{ display: 'flex' }}>
+          <div style={{ flex: '1' }}>
+            <p>
+              The project will develop scientific knowledge to support pathways toward sustainable agricultural water management in the Hindon basin and create accurate/relevant modeling approaches regarding <br /> <br />
+
+              (a) the application of remote sensing technologies for high-resolution physical soil, chemical, and groundwater properties <br /> <br />
+
+              (b) eco-hydrological modeling at the field/farmer level <br /> <br />
+
+              (c) integrated modeling from the field to the basin <br /> <br />
+
+              The innovation will focus on investigating scientific solutions that create details for eco-agro-water spatial planning with minimal need for ground data.
+            </p>
+          </div>
+          <div style={{ flex: '1' }}>
+            <img style={{ maxWidth: '100%' }} src={require('../assets/hiros-project.png')} alt="" />
+          </div>
+        </div>
+        <p className="section-title" style={{ fontSize: '20px' }}>Project Summary</p>
+        <p>The intensification of extreme weather and climatic events, evident in the recent past, has resulted in severe damage/failure of the water resource infrastructure. These infrastructures are not expected to be resilient to the changing climate, as they were planned and designed based on the conventional stationarity assumption. Hence, several studies have recommended the idea of capturing the temporal dynamics (i.e., non-stationarity) in extremes. Nonstationary modelling developed merely by addressing the changes in extremes over time, without understanding the physical interpretation, results in an unreliable future prediction. Without incorporating the physical factors, Nonstationary modelling may produce more uncertainty than stationary modelling. This study investigates the physical relevance behind the changes in flood characteristics and develops a robust approach to estimate the design flood through the multivariate Flood Frequency Analysis (FFA) for a changing climate and environment. The signature of both long-term and short-term non-stationarity is detected in the flood peak, flood volume and flood duration. Further, the physical factors (i.e., process-informed covariates) that influence these changes will be identified, and a suitable covariate-based model will be developed to perform nonstationary FFA. The design flood and joint return period for major riverine flood basins in India will be estimated, providing information for the design and operation of hydraulic structures incorporating flood risk. It will also pave the way for further research in River flood management, dynamic reservoir management and flood forecasting under a changing climate and environment.</p>
+        <p className="section-title" style={{ fontSize: '20px' }}>Objectives</p>
+        <p>The following objectives are formulated to address the gap in the literature:
+          <ol>
+            <li>
+              Detect and attribute the temporal short-term and long-term changes (if any) in the extreme streamflow characteristics (flood peak, flood volume and flood duration).
+            </li>
+            <li>
+              Examine the flood frequency characteristics through exploring the effect of non-stationarity through identified process-informed covariates and interdependency between the flood characteristics.
+            </li>
+            <li>
+              Investigate the occurrence and distribution of flood events for future climate, considering the projections of process-informed covariates from various general circulation models, land change models and different future scenarios.
+            </li>
+            <li>
+              Formulate possible methodologies to quantify or even reduce the uncertainty to improve the reliability of design flood estimation under the nonstationary condition
+            </li>
+          </ol>
+        </p>
+        <p className="section-title" style={{ fontSize: '20px' }}>Keywords</p>
+        <p>Design flood, nonstationarity model, joint return period, frequency analysis, climate change</p>
+        <p className="section-title" style={{ fontSize: '20px' }}>Sponsers</p>
+        <br />
+        <div style={{ display: 'flex' ,height:'160px',justifyContent:'space-evenly'}}>
+          <div>
+            <img style={{maxWidth:'100%',maxHeight:'100%',marginInline:'auto'}} src={require('../assets/logo/dst-logo.webp')} alt="" />
+          </div>
+          <div>
+            <img style={{maxWidth:'100%',maxHeight:'100%',marginInline:'auto'}} src={require('../assets/logo/nwo-1-logo.png')} alt="" />
+          </div>
+        </div>
+        <br /><br />
+        <ul>
+          <li>
+            Hydrological Modelling for Dynamic Catchments (2021): Funded by Sponsored Research & Industrial Consultancy Office, Indian Institute of Technology Roorkee. Budget: ₹21.6 Lakhs
+          </li>
+          <li>
+            Design flood estimation for resilient hydraulic structures under changing climate and environments (2022): Funded by SERB India under Startup Research Grant Scheme. Budget: ₹19.6 Lakhs
+          </li>
+        </ul>
+        <li><b style={{ fontSize: '1.4rem' }}>Hydrological Modelling for Dynamic Catchments (2021):</b>
+          <br />
+          Funded by Sponsored Research & Industrial Consultancy Office, Indian Institute of Technology Roorkee. Budget: ₹21.6 Lakhs</li> <br />
+        <li><b style={{ fontSize: '1.4rem' }}>Design flood estimation for resilient hydraulic structures under changing climate and environments (2022): </b>
+          <br />
+          Funded by SERB India under Startup Research Grant Scheme. Budget: ₹19.6 Lakhs</li> <br />
       </ul>
       {/* <p className="section-title">Invited Talks / Guest Lectures</p>
       <ul className="text-1 list-1" style={{listStyle:'circle',fontSize:'1rem'}}>
